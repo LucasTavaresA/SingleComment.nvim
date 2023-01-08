a291d87c358a06e429769f0440ed906d79116834
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-01-08 16:50:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# SingleComment.nvim
+
+Super simple comment plugin that always uses single line comments
+
+## Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Keybindings](#keybindings)
+- [Lazy load it](#lazy-load-it)
+
+## Features
+
+1. Always comments single lines, **Never get confused again** XD
+
+2. Supports counts like 5gcc and commenting in front of the current line
+
+3. Simplest plugin of them all, it uses a single function for everything, no
+   fancy features, can be lazy-loaded very easily
+
+4. Has great language support with a lua table made by hand, this is empowered by single line comments,
+   **please help adding more languages**
+
+## Installation
+
+[packer.nvim](https://github.com/wbthomason/packer.nvim):
+
+```lua
+use "lucastavaresa/SingleComment.nvim"
+```
+
+[lazy.nvim](https://github.com/folke/lazy.nvim):
+
+```lua
+"lucastavaresa/SingleComment.nvim"
+```
+
+## Keybindings
+
+You need to set those to use SingleComment.nvim
+
+Those are all the available functions:
+
+```lua
+vim.keymap.set({ "n", "v" }, "gcc", require("SingleComment").SingleComment)
+vim.keymap.set("n", "gca", require("SingleComment").SingleCommentAhead)
+```
+
+## Lazy load it
+
+Those commands substitute all the above
+
+- with packer **i did not test this, please inform me if it works** :)
+
+```lua
+  use {
+    "lucastavaresa/SingleComment.nvim",
+    opt = true,
+    keybindings = { { { "n", "v" }, "gcc" }, { "n", "gca" } },
+    setup = function()
+      vim.keymap.set({ "n", "v" }, "gcc", require("SingleComment").SingleComment)
+      vim.keymap.set("n", "gca", require("SingleComment").SingleCommentAhead)
+    end
+  }
+```
+
+- with lazy:
+
+```lua
+  {
+    "lucastavaresa/SingleComment.nvim",
+    lazy = true,
+    keys = {
+      {
+        "gcc",
+        function()
+          require("SingleComment").SingleComment()
+        end,
+        mode = { "n", "v" },
+      },
+      {
+        "gca",
+        function()
+          require("SingleComment").SingleCommentAhead()
+        end,
+      },
+    },
+  },
+```
---
+++ lua/SingleComment.lua
@@ -0,0 +1,89 @@
+---@type table
+local M = {}
+---@type table
+local blockcomments = require("SingleComment.kinds").blockcomments
+---@type table
+local comments = require("SingleComment.kinds").comments
+
+--- inserts a comment at the end of the current line
+function M.SingleCommentAhead()
+  local comment
+  if blockcomments[vim.o.ft] ~= nil then
+    comment = blockcomments[vim.o.ft]
+  else
+    comment = comments[vim.o.ft]
+  end
+
+  local line = vim.api.nvim_get_current_line()
+    .. " "
+    .. comment[1]
+    .. (comment[2] or "")
+
+  vim.api.nvim_set_current_line(line)
+
+  if comment[2] ~= nil then
+    vim.api.nvim_input("A" .. string.rep("<left>", #comment[2]))
+  else
+    vim.api.nvim_input("A ")
+  end
+end
+
+--- comments single lines
+function M.SingleComment()
+  local comment
+  if blockcomments[vim.o.ft] ~= nil then
+    comment = blockcomments[vim.o.ft]
+  else
+    comment = comments[vim.o.ft]
+  end
+
+  local count = vim.v.count
+  local col = vim.fn.col(".") - 1
+  local startRow, endRow = vim.fn.line("v"), vim.fn.line(".")
+  local pos = { startRow, col }
+
+  -- in case the selection starts from the bottom
+  if startRow > endRow then
+    local tmp = startRow
+    startRow = endRow
+    endRow = tmp
+    pos = { endRow, col }
+  end
+
+  if count ~= 0 then
+    endRow = endRow + count - 1
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(0, startRow - 1, endRow, true)
+  local indent = string.match(lines[1], "^%s*")
+
+  local uncomment
+  for i, _ in ipairs(lines) do
+    if not lines[i]:match("^%s*$") then
+      if not lines[i]:match("^" .. indent .. vim.pesc(comment[1])) then
+        uncomment = true
+        break
+      end
+    end
+  end
+
+  for i, _ in ipairs(lines) do
+    if not lines[i]:match("^%s*$") then
+      lines[i] = string.gsub(lines[i], "^" .. indent, "")
+
+      if not uncomment then
+        lines[i] = lines[i]
+          :gsub("^" .. vim.pesc(comment[1]), indent)
+          :gsub(vim.pesc(comment[2] or "") .. "$", "")
+      else
+        lines[i] = indent .. comment[1] .. lines[i] .. (comment[2] or "")
+      end
+    end
+  end
+
+  vim.api.nvim_buf_set_lines(0, startRow - 1, endRow, true, lines)
+  vim.api.nvim_input("<esc>")
+  vim.api.nvim_win_set_cursor(0, pos)
+end
+
+return M
